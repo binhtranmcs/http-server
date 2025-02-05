0de375dea5d431f7eae59aa3c354632277f5aed6
binhtt4 <binhtt4@vng.com.vn>
2025-02-05 16:36:35 +0700

    update logging with time

--- src/logging.h
+++ src/logging.h
@@ -6,24 +6,45 @@
 #define HTTP_SERVER_LOGGING_H
 
 
+#include <chrono>
 #include <cstring>
+#include <iomanip>
+#include <iostream>
+
+
+inline std::string Timestamp() {
+  auto now = std::chrono::system_clock::now();
+  auto epoch = now.time_since_epoch();
+  auto sec = duration_cast<std::chrono::seconds>(epoch);
+  auto ns = duration_cast<std::chrono::nanoseconds>(epoch - sec);
+
+  std::time_t now_c = std::chrono::system_clock::to_time_t(now);
+  std::tm now_tm = *std::localtime(&now_c);
+
+  std::ostringstream oss;
+  oss << std::put_time(&now_tm, "%Y-%m-%d %H:%M:%S") << "." << std::setfill('0') << std::setw(9)
+      << ns.count();
+  
+  return oss.str();
+}
 
 
 #define FILENAME (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 
 
-#define LOG(level, msg)                                                         \
-  do {                                                                          \
-    std::cout << "[" << (level) << "] [" << FILENAME << ":" << __LINE__ << "] " \
-              << msg /*NOLINT*/ << std::endl;                                   \
+#define LOG(level, msg)                                                                          \
+  do {                                                                                           \
+    Timestamp();                                                                                 \
+    std::cout << Timestamp() << "\t[" << (level) << "] [" << FILENAME << ":" << __LINE__ << "] " \
+              << msg /*NOLINT*/ << std::endl;                                                    \
   } while (0)
 
 
 // always define log error
-#define LOG_ERROR(msg)                                              \
-  do {                                                              \
-    std::cerr << "[ERROR] [" << FILENAME << ":" << __LINE__ << "] " \
-              << msg /*NOLINT*/ << std::endl;                       \
+#define LOG_ERROR(msg)                                                               \
+  do {                                                                               \
+    std::cerr << Timestamp() << "\t[ERROR] [" << FILENAME << ":" << __LINE__ << "] " \
+              << msg /*NOLINT*/ << std::endl;                                        \
   } while (0)
 
 #if defined(LOG_LEVEL_DEBUG)
--- src/protocol_handler.cc
+++
@@ -1,5 +0,0 @@
-//
-// Created by binhtt4 on 04/02/25.
-//
-
-#include "protocol_handler.h"
